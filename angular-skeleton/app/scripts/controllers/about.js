4a61da19f2e3d1835b5390c247f7948bbe919aa5
Администратор <@WIN-GKGKJEA31T2>
2017-06-05 16:57:29 +0300

    Initial commit

---
+++ angular-skeleton/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name helloangularApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the helloangularApp
+ */
+angular.module('helloangularApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ angular-skeleton/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name helloangularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the helloangularApp
+ */
+angular.module('helloangularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
