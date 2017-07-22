fbcbc78e7aa277be49e6778cf709e440e8075730
Dore Mioto <dore.mioto@gmail.com>
2017-07-22 20:52:39 +0300

    Working on actionPerformer

--- app/scripts/battleground.engine/battleground.engine.module.js
+++ app/scripts/battleground.engine/battleground.engine.module.js
@@ -47,13 +47,19 @@ angular
 			  var defer = $q.defer();
 			  var unitTeamId = 0, battlegroundId = 0, // TODO: later these will be taken using Security Token;
 			      battleground = $battlegroundDBConnector.getBattleground(0),
+			      actorUnit = battleground.battleUnits.filter(function(battleUnit){
+					  return battleUnit.team === unitTeamId && battleUnit.number === unitId;
+				  })[0],
 			      result = {};
-			  if(!isTeamCanAct(unitTeamId, battleground, result)){
+			  if(!isTeamCanAct(unitTeamId, battleground, result) ||
+					  !isUnitCanAct(unitId, battleground, result) ||
+					  !isActionAvailable(actorUnit, actionId, result)){
+				  defer.resolve(result);
+			  } else {
+				  result.message = 'Action #' + actionId + ' performed by ' + unitId + ' to ' + targetId + ' from team #' + targetTeamId;
+				  result.battlegroundState = $battlegroundDBConnector.getBattleground(0);
 				  defer.resolve(result);
 			  }
-			  result.message = 'Action #' + actionId + ' performed by ' + unitId + ' to ' + targetId + ' from team #' + targetTeamId;
-			  result.battlegroundState = $battlegroundDBConnector.getBattleground(0);
-			  defer.resolve(result);
 			  return defer.promise;
 		  }
 	  }
@@ -64,6 +70,29 @@ angular
 		  } else {
 			  result.errorCode = 1;
 			  result.errorMessage = 'Your team cant move right now';
+			  return false;
+		  }
+	  }
+	  
+	  function isUnitCanAct(unitId, battleground, result){
+		  var isCanAct = true;// TODO: Later here we will check if unit is stunned
+		  if(isCanAct){
+			  return true;
+		  } else {
+			  result.errorCode = 2;
+			  result.errorMessage = 'Your unit cant move right now';
+			  return false;
+		  }
+	  }
+	  
+	  function isActionAvailable(actorUnit, actionId, result){
+		  var actionMeta = actorUnit.skills[actionId];
+		  if(actionMeta){
+			  console.log(actionMeta);
+			  return true;
+		  } else {
+			  result.errorCode = 3;
+			  result.errorMessage = 'Your skill is unavailable';
 			  return false;
 		  }
 	  }
